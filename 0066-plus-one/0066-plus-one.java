6784fb6e1323a0fd193dbe5b07f8f511a6fe9a8c
salonireddy <83425697+salonireddy@users.noreply.github.com>
2023-12-23 23:06:24 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (5.77%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,9 +2,8 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int flag = 0;
         int i= digits.length-1;
-        for(;digits[i]==9;){
+        while(digits[i]==9){
             flag = 1;
-            System.out.print("here1");
             if(i>=0) digits[i] = 0;
             if (i<=0) break;
             i--;
@@ -15,12 +14,10 @@ class Solution {
         }
         
         if(flag==1 && i==0){
-            System.out.print("here4");
             int[] res = new int[digits.length+1];
             res[0]=1;
             for(int j=0,k=0;j<res.length-1;j++,k++){
                 res[j+1]=digits[k];
-                System.out.print("here2");  
             }
             return res;
         }
@@ -28,7 +25,6 @@ class Solution {
             digits[i]=digits[i]+1;
             return digits;
         }
-        System.out.print("here3");
 
         digits[digits.length - 1] = digits[digits.length - 1] + 1;
         return digits;
